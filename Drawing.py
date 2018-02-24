223f348c13471f90e2241a88d0f8055119376ee4
a.shibaev <a.shibaev@intelcomline.ru>
2018-02-24 08:40:25 +0300

    Initial commit

---
+++ Drawing.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QLabel, QWidget)
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtCore import Qt
+
+class MouseTracker(QWidget):
+    distance_from_center = 0
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setMouseTracking(True)
+
+    def initUI(self):
+        self.setGeometry(200, 200, 1000, 500)
+        self.setWindowTitle('Mouse Tracker')
+        self.label = QLabel(self)
+        self.label.resize(500, 40)
+        self.show()
+        self.pos = None
+
+    def mouseMoveEvent(self, event):
+        distance_from_center = round(((event.y() - 250)**2 + (event.x() - 500)**2)**0.5)
+        self.label.setText('Coordinates: ( %d : %d )' % (event.x(), event.y()) + "Distance from center: " + str(distance_from_center))
+        self.pos = event.pos()
+        self.update()
+
+
+app = QApplication(sys.argv)
+ex = MouseTracker()
+sys.exit(app.exec_())
